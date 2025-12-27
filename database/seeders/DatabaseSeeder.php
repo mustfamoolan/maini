0f96300a387962d663e71216151e95306fb126e1
mustfamoolan <almoq3m@gmail.com>
2025-12-27 17:28:36 +0300

    Fix database seeder to use AdminUserSeeder and make phone field nullable

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,14 +17,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Larkon',
-            'email' => 'test@example.com',
-            'email_verified_at' => now(),
-            'password' => Hash::make('password'),
-            'remember_token' => Str::random(10),
+        // Call AdminUserSeeder to create admin user
+        $this->call([
+            AdminUserSeeder::class,
         ]);
     }
 }
